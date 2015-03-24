4d5bbc8ae81e983eebe29e94b9ff3f77e7191149
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-03-24 07:26:39 +0000

    changed wordign in about paragraph

--- resources/views/partials/aboutthisproject.blade.php
+++ resources/views/partials/aboutthisproject.blade.php
@@ -2,7 +2,7 @@
 	
 	<h4>About This Project</h4>
 	<p>
-		This tool makes use of the <a href="http://nouwweb.readthedocs.org/en/latest/">Nouwweb Api</a> to get the latest tweets from the Lebanese Members of Parliament, filtered by relevant criteria.
+		This tool builds on top of the <a href="http://nouwweb.readthedocs.org/en/latest/">Nouwweb Api</a> to get the latest tweets from the Lebanese Members of Parliament, filtered by relevant criteria.
 		<br>
 		For more info <a href="http://twitter.com/beirutspring">contact me on Twitter</a>;
 	</p>
