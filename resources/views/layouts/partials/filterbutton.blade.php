bddc4c90bbece7a3ecac2de75f30661fd0899d8b
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-04-13 22:45:04 +0000

    fixed Case bug for server

--- resources/views/layouts/partials/filterbutton.blade.php
+++ resources/views/layouts/partials/filterbutton.blade.php
@@ -1,5 +1,5 @@
 
 <div class="inner filterButton">
 	<h5 id="tryme">Try me &rarr;</h5>
-	<button>@include('svgicons.filter') Filters</button>
+	<button>@include('svgIcons.filter') Filters</button>
 </div>
