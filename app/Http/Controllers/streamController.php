0b5ca20021ee61115309084b21b8f14bb3fa23c2
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-04-27 17:38:15 +0000

    testing why categories are not working

--- app/Http/Controllers/streamController.php
+++ app/Http/Controllers/streamController.php
@@ -10,7 +10,7 @@ use LebaneseTweets\Tweep;
 
 class streamController extends Controller {
 
-	private $allowedGroups = ['politicians','journalists'];
+	private $allowedGroups = ['politicians','journalists', 'politicianstest'];
 
 	public function index(Request $request, $group = null){
 
@@ -21,6 +21,10 @@ class streamController extends Controller {
 			return \Redirect::to('/');
 		}
 		
+		if ($group == 'politicianstest') {
+			$tweets = (New Tweet)->makeQuery('politicians', 0, 20, $request);
+			return $tweets;
+		}
 		$tweets = (New Tweet)->makeQuery($group, 0, 20, $request);
 
 		return view('layouts.main')->with(array('tweets'=>$tweets, 'request'=>$request, 'group'=>$group));
