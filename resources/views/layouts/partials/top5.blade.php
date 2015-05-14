95a60d01085658ebc583c07ce65f7a3b9abac66c
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-05-14 19:48:37 +0000

    improved look of top tweets

--- resources/views/layouts/partials/top5.blade.php
+++ resources/views/layouts/partials/top5.blade.php
@@ -10,44 +10,38 @@
 			// Detect language
 			$isArabic = \LebaneseTweets\Utilities\String::isMostlyArabic($topTweet->content);
 		?>
-		<li>
-		<a href="https://twitter.com/{{$topTweet->tweep_twitterHandle}}/status/{{$topTweet->twitter_id}}">
-			<div class="item">
-				<div class="thumb">
-					@if($topTweet->image())
-						@if($topTweet->image_height() > $topTweet->image_width())
-							<img src ="{{$topTweet->image()}}" width="{{$thumbwidth}}" height="auto">
-						@else
-							<img src ="{{$topTweet->image()}}" height="{{$thumbwidth}}" width="auto">
-						@endif
-					@else
-						<img src="{{\URL::to('/').'/img/no-image.png'}}" width="{{$thumbwidth}}" height="auto">
-					@endif
-				</div>
-				<div class="details  @if($isArabic) arabic @endif">
-					<img class="profile" src="{{$topTweet->user_image}}" height="15" width="15" alt="">
-					<h5> {{$topTweet->tweep_public_name}}</h5>
-					<?php echo strip_tags($topTweet->content) ?>
-					<ul class="retweets_and_favorites">
-						<li class="retweets">@include('svgIcons.retweet'){{$topTweet->retweets}}</li>
-						<li class="favorites">@include('svgIcons.favorite'){{$topTweet->favorites}}</li>
-					</ul>
-				</div>
-			</div>
-		</a>
+		<li class="tweet">
+        <div class="profilepic">
+          <a href="https://twitter.com/{{$topTweet->tweep_twitterHandle}}/status/{{$topTweet->twitter_id}}">
+            <img src="{{$topTweet->user_image}}">
+          </a>
 
-			<div>
-				{{-- 
-					<img class="profile" src="{{$topTweet->user_image}}" height="12" width="12" alt="">
-					<h4>{{$topTweet->tweep_public_name}}</h4>
-					
-					
-				--}}
-				
-			</div>
-
-
-		</li>
+        </div>
+        <div class="content">
+          <h5><strong>{{$topTweet->tweep_public_name}}</strong> {{'@'.$topTweet->tweep_twitterHandle}}</h5>
+          <p @if($isArabic) class="arabic" @endif>{!!$topTweet->content!!}</p>
+          @if($topTweet->media)
+            <div class="media">
+               <a href="https://twitter.com/{{$topTweet->tweep_twitterHandle}}/status/{{$topTweet->twitter_id}}">
+                <img src="{{$topTweet->media}}" width="{{$topTweet->media_width}}" height="auto">
+              </a>
+            </div>
+          @elseif($topTweet->link)
+            @if($topTweet->link->image)
+            <div class="media">
+               <a href="https://twitter.com/{{$topTweet->tweep_twitterHandle}}/status/{{$topTweet->twitter_id}}">
+                <img src="http://lebanesetweets.net/{{$topTweet->link->image}}" width="{{$topTweet->link->image_width}}" height="auto">
+              </a>
+            </div>
+            @endif
+          @endif
+          <ul class="popularity">
+            <li><a href="https://twitter.com/{{$topTweet->tweep_twitterHandle}}/status/{{$topTweet->twitter_id}}">@include('svgIcons.link') link</a></li>
+            <li>@include('svgIcons.favorite') {{$topTweet->favorites}}</li>
+            <li>@include('svgIcons.retweet') {{$topTweet->retweets}}</li>
+          </ul>
+        </div>
+      </li>
 		@endforeach
 	</ul>
 </li>
